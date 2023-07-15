92a185ff1502acebf3695b06f26c46cd6a027b45
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-07-15 21:26:29 +0300

    Player Controller created and input System
    Input system and player addforce

--- takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using takerocket.Inputs;
 using UnityEngine;
 
 namespace takerocket.Controllers//Klasörleme mantığı oyunun_adi.bulunduğu klasör adı
@@ -7,16 +8,45 @@ namespace takerocket.Controllers//Klasörleme mantığı oyunun_adi.bulunduğu k
     public class PlayerController : MonoBehaviour
     {
 
+        Rigidbody _rigidbody;
+        DefaultInput _input;
+        bool _isForceUp;
+        [SerializeField] float force;
+        
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _input = new DefaultInput();
+        }
+
+        //Input işlemleri
+        private void Update()
+        {
+            Debug.Log(_input.IsForceUp);
+            if (_input.IsForceUp)
+            {
+                _isForceUp = true;
+            }
+            else
+            {
+                _isForceUp = false;
+            }
+        }
+
+        //Fizik işlemleri
+        private void FixedUpdate()
+        {
+            if (_isForceUp)
+            {
+                _rigidbody.AddForce(Vector3.up * force * Time.deltaTime );
+            }
+        }
 
 
 
 
-
-
-
-
-
-
+       
 
     }//class
 
---
+++ takerocket/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultInput.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace takerocket.Inputs
+{
+    public class DefaultInput
+    {
+        DefaultAction _input;
+        public bool IsForceUp { get; private set; }
+
+
+        public DefaultInput()
+        {
+            _input = new DefaultAction();
+
+            _input.Rocket.ForceUp.performed += context => IsForceUp = context.ReadValueAsButton();//çok satır ise {} açılır tek satırsa gerek yok burada şunu yapıyoruz diyoruz ki eğer bu aksiyon çalıştıysa burdan gelen veriyi context değişkenine ata ve IsForceUp değişkenine bu contextin buttonb valuesini ver butopna tıklandıysa button value true değilse false gelir
+
+            _input.Enable();//Input un çalışması için önemli!!
+        }
+
+       
+
+    }
+}
+
