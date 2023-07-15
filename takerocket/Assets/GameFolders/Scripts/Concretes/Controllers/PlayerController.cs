834220f999a530afce1cf1c49ea4ee2a43aa9d84
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-07-15 19:48:30 +0300

    Player Component1
    Player add script and box collider component

---
+++ takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace takerocket.Controllers//Klasörleme mantığı oyunun_adi.bulunduğu klasör adı
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+    }//class
+
+}
