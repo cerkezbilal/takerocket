eb638e4498f010ee92b4614f73e62fd40fe75dcb
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-07-17 13:25:12 +0300

    FinishFloorController created
    Controller işlemleri-1

---
+++ takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace takerocket.Controllers
+{
+    public class FinishFloorController : MonoBehaviour
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//class
+}
+
