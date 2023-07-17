7a040be0ee0034dc6ad743959e400e4873ca58da
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-07-17 13:50:10 +0300

    FinishFloorController2
    FinisFloor işlemlerini tamamladık

--- takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
+++ takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace takerocket.Controllers
 {
     public class FinishFloorController : MonoBehaviour
     {
 
+        [SerializeField] GameObject _finishFireWorks;
+        [SerializeField] GameObject _finisLight;
 
 
+        //Roket finishFloor a tepeden değdiğinde efektler çalışacak.
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            PlayerController player = collision.collider.GetComponent<PlayerController>();
+
+            if (player == null) return;
+
+            //İlk değdiği demek oluyor tepeden ya da ya da aşağıda değebilir. tepeden aşağıya demek bu 
+            if (collision.GetContact(0).normal.y == -1)
+            {
+                //Efektleri göster demek
+                _finishFireWorks.gameObject.SetActive(true);
+                _finisLight.gameObject.SetActive(true);
+            }
+            else
+            {
+                //GameOver işlemi
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            
+        }
 
 
 
