4aeb913784004c483c8c62fd6b0620c0cbcc3ec1
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-07-19 13:03:09 +0300

    Levels Design
    8 level ekledik

--- takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
+++ takerocket/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
@@ -8,11 +8,10 @@ namespace takerocket.Controllers
     public class MoverWallController : WallController
     {
         [SerializeField] Vector3 _direction;//Yön
-
-        
-        [SerializeField] float _faktor;//Sınır verecek bize 0 ile 1 arasında gidip gelecek
-
         [SerializeField] float _speed = 1f;
+        //[SerializeField] float _directionFaktor = 1f;
+
+        float _faktor;//Sınır verecek bize 0 ile 1 arasında gidip gelecek
 
         const float FULL_CYCLE = Mathf.PI * 2f;
 
@@ -30,11 +29,12 @@ namespace takerocket.Controllers
             float cycle = Time.time / _speed;//yani zamanı biraz yavaşlatıcaz ki o hız sorunu gitsin
             float sinWave =Mathf.Sin(cycle * FULL_CYCLE);
 
-            _faktor = Mathf.Abs(sinWave);
+            // _faktor = Mathf.Abs(sinWave);
+            _faktor = sinWave / 2 + 0.5f; //sinwave -1 +1 arasında verdiği için 2 ye böşüp 0.5f verince dengelenmiş oluyor.
 
             
             //yönü aldık gelen değer ile çarptım
-            Vector3 offset = _direction * _faktor;
+            Vector3 offset = _direction * _faktor ;
 
              //gelen değer ile ilk pozisyonu toplayıp pozisyona verdim
              transform.position = offset + _startPosition;
